f94539722b2952d44553915a9820238050528302
Mariam Iftikhar <mariamiftikhar127@gmail.com>
2025-04-08 10:37:01 -0500

    Initial Commit

---
+++ PackageInstallationTask-Easy.R
@@ -0,0 +1,16 @@
+# Easy Task: Package Installation
+
+library(remotes)
+install_github("JustinMShea/ExpectedReturns")
+install_github("braverock/FactorAnalytics")
+install_github("braverock/FactorAnalytics", force = TRUE)
+
+
+# Conclusion:
+
+# •	ExpectedReturns and FactorAnalytics installed successfully.
+# •	Some dependencies had permission issues, but that’s fixable with a restart or elevated privileges.
+# •	No actual errors stopped the process — only warnings.
+
+
+
